b460bf8abc78b893c8600e1aa7234bb54d3f74dd
Ihor Shkliar <igor.shklyar@gmail.com>
2020-05-18 23:05:22 +0300

    111. Using "props" in the Child Component

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,7 +3,7 @@
         <div class="component">
             <h3>You may view the User Details here</h3>
             <p>Many Details</p>
-            <p>User Name: {{ name }}</p>
+            <p>User Name: {{ switchName() }}</p>
             <p>My Name: {{ myName }}</p>
         </div>
     </div>
@@ -11,7 +11,12 @@
 
 <script>
     export default {
-        props: ['name', 'myName']
+        props: ['name', 'myName'],
+        methods: {
+            switchName() {
+                return this.name.split("").reverse().join("");
+            }
+        }
     }
 </script>
 
